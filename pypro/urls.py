6601b19ee5ab1573d65363880837438cce31f1fc
Marco <miranda_garcya@hotmail.com>
2021-03-18 15:59:41 -0300

    Criada  Landing Page
    close #127

--- pypro/urls.py
+++ pypro/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
-
-from pypro.base.views import home
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('pypro.base.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(
+        path('__debug__/', include(debug_toolbar.urls))
+    )
