81258c715d05abbc77a9a81c21ffdcf87c54b3b5
Marco <miranda_garcya@hotmail.com>
2021-03-27 09:59:25 -0300

    Integrado videos promocionais close #148

---
+++ pypro/aperitivos/tests.py
@@ -0,0 +1,21 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivaçao</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<iframe width="560" height="315" src="https://www.youtube.com/embed/2aYplgJrPDU"')
