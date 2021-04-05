267db8c092d652658982f8ccab5afff52dd06247
Marco <miranda_garcya@hotmail.com>
2021-04-05 14:27:16 -0300

    Criado página para segundo video aperitivo close #168

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos = {
+        'motivacao':{'titulo': 'Video Aperitivo: Motivação', 'youtube_id': '2aYplgJrPDU'},
+    }
+    video = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
