856b93572340878f02dcd7691e0cbb93d36a1093
Marco <miranda_garcya@hotmail.com>
2021-04-06 16:37:05 -0300

    Crieado lista de elementos com for close #176

---
+++ pypro/aperitivos/tests/tests_indice.py
@@ -0,0 +1,28 @@
+import pytest
+
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:indice'))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+@pytest.mark.parametrize(
+    'titulo',
+    [
+        'Video Aperitivo: Motivação',
+    ]
+)
+def test_titulo_video(resp, titulo):
+    assert_contains(resp, titulo)
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<iframe width="560" height="315" src="https://www.youtube.com/embed/2aYplgJrPDU"')
