186e428e4946603e2c3ac6ef115e3a7d5f492d5e
Marco <miranda_garcya@hotmail.com>
2021-05-07 13:19:08 -0300

    Finalizado Teste de Video e Criação de Migração close #247

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,14 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from pypro.aperitivos.models import Video
 
-videos = [
-    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', youtube_id='2aYplgJrPDU'),
-]
-
-videos_dct = {v.slug: v for v in videos}
-
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
