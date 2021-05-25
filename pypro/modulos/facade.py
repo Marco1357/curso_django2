b3e5f6cca64e0791fc56e8df347bf4f1c2b7bc9f
Marco <miranda_garcya@hotmail.com>
2021-05-25 15:50:00 -0300

    Criada Página de Detalhe do módulo close #282

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -2,11 +2,15 @@ from typing import List
 
 from pypro.modulos.models import Modulo
 
+"""
+   Lista módulos ordenados por títulos
+   :return:
+   """
+
 
 def listar_modulos_ordenados() -> List[Modulo]:
-    """
-    Lista módulos ordenados por títulos
-    :return:
-    """
-
     return list(Modulo.objects.order_by('order').all())
+
+
+def encontrar_modulo(slug: str) -> Modulo:
+    return Modulo.objects.get(slug=slug)
--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from ordered_model.models import OrderedModel
+from django.urls import reverse
 
 
 class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
     publico = models.TextField()
     descricao = models.TextField()
+    slug = models.SlugField(unique=True)
 
     class Meta(OrderedModel.Meta):
         pass
 
     def __str__(self):
         return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})
