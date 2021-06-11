a22dbda28e96ca88e9fdb1cfdcf79883ca128350
Marco <miranda_garcya@hotmail.com>
2021-06-10 22:14:01 -0300

    Criado Funcionalidade de Indice de Módulos close #322

---
+++ pypro/modulos/tests/test_modulos_indice.py
@@ -0,0 +1,54 @@
+from typing import List
+import pytest
+from django.urls import reverse
+from model_mommy import mommy
+from pypro.django_assertions import assert_contains
+from pypro.modulos.models import Modulo, Aula
+
+
+@pytest.fixture
+def modulos(db):
+    return mommy.make(Modulo, 2)
+
+
+@pytest.fixture
+def aulas(modulos):
+    aulas = []
+    for modulo in modulos:
+        aulas.extend(mommy.make(Aula, 3, modulo=modulo))
+        return aulas
+
+
+@pytest.fixture
+def resp(client, modulos, aulas):
+    resp = client.get(reverse('modulos:indice'))
+    return resp
+
+
+def test_indice_disponivel(resp):
+    assert resp.status_code == 200
+
+
+def test_titulo(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.titulo)
+
+
+def test_descricao(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.descricao)
+
+
+def test_publico(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.publico)
+
+
+def test_aulas_titulos(resp, aulas: List[Aula]):
+    for aula in aulas:
+        assert_contains(resp, aula.titulo)
+
+
+def test_aulas_url(resp, aulas: List[Aula]):
+    for aula in aulas:
+        assert_contains(resp, aula.get_absolute_url())
--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-
+#from django.contrib.auth.decorators import login_required
 from pypro.modulos import facade
+
+
+def indice(request):
+    ctx = {'modulo': facade.listar_modulo_com_aula()}
+    return render(request, 'modulos/indice.html', ctx)
 
 
 def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
-    aulas = facade.listar_aula_de_modulo_ordenadas(modulo)
+    aulas = facade.listar_aulas_de_modulos_ordenados(modulo)
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
-
-
-def aula(request, slug):
-    aula = facade.encontrar_aula(slug)
-    return render(request, 'modulos/aula_detalhe.html', {'aula': aula})
