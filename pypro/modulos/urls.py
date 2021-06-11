ebe3354c8cfc0e88900efbc6d07b5f76502c58f8
Marco <miranda_garcya@hotmail.com>
2021-06-10 22:23:37 -0300

    Criado Funcionalidade de Indice de Módulos close #322

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -5,5 +5,6 @@ from pypro.urls import path
 app_name = 'modulos'
 urlpatterns = [
     path('<slug:slug>', views.detalhe, name='detalhe'),
+    path('aulas/<slug:slug>', views.aula, name='aula'),
     path('', views.indice, name='indice'),
 ]
--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-#from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from pypro.modulos import facade
 
 
 def indice(request):
-    ctx = {'modulo': facade.listar_modulo_com_aula()}
+    ctx = {'modulos': facade.listar_modulo_com_aulas()}
     return render(request, 'modulos/indice.html', ctx)
 
 
@@ -12,3 +12,9 @@ def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.listar_aulas_de_modulos_ordenados(modulo)
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
+
+
+@login_required
+def aula(request, slug):
+    aula = facade.encontrar_aula(slug)
+    return render(request, 'modulos/aula_detalhe.html', {'aula': aula})
