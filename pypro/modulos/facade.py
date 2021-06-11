7ee272bc3050a3b735619211d49f3bc2fd3aefc8
Marco <miranda_garcya@hotmail.com>
2021-06-10 22:14:01 -0300

    Criado Funcionalidade de Indice de Módulos close #322

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from django.db.models import Prefetch
+
 from pypro.modulos.models import Modulo, Aula
 
 """
@@ -17,9 +19,15 @@ def encontrar_modulo(slug: str) -> Modulo:
     return Modulo.objects.get(slug=slug)
 
 
-def listar_aula_de_modulo_ordenadas(modulo: Modulo):
-    return list(modulo.aula_set.order_by('order').all())
-
-
 def encontrar_aula(slug):
     return Aula.objects.select_related('modulo').get(slug=slug)
+
+
+def listar_aulas_de_modulos_ordenados(modulo: Modulo):
+    return list(modulo.aula_set.order_by('modulo').all())
+
+
+def listar_modulo_com_aula():
+    aulas_ordenadas=Aula.objects.order_by('order')
+    return Modulo.objects.order_by('order').prefetch_related(
+        Prefetch('aula_set', queryset=aulas_ordenadas,to_attr='aulas')).all()
