2d9118f1a09cb474dc05215d5da61027ca7beb3b
Marco <miranda_garcya@hotmail.com>
2021-06-18 13:11:53 -0300

    Criado Formulário de Login close #339

---
+++ pypro/base/tests/test_login.py
@@ -0,0 +1,12 @@
+import pytest
+
+from django.urls import reverse
+
+
+@pytest.fixture
+def resp(client, db):
+    return client.get(reverse('login'))
+
+
+def test_login_for_page(resp):
+    assert resp.status_code == 200
