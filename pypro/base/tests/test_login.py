66f89df9612839be0bb9b397e1a88d2f3af35495
Marco <miranda_garcya@hotmail.com>
2021-06-21 13:41:27 -0300

    Criado Botão de Login close #341

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -1,6 +1,8 @@
 import pytest
-
+from model_mommy import mommy
 from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
@@ -10,3 +12,36 @@ def resp(client, db):
 
 def test_login_for_page(resp):
     assert resp.status_code == 200
+
+
+@pytest.fixture
+def usuario(db, django_user_model):
+    usuario_modelo = mommy.make(django_user_model)
+    senha = 'senha'
+    usuario_modelo.set_password(senha)
+    usuario_modelo.save()
+    usuario_modelo.senha_plana = senha
+    return usuario_modelo
+
+
+@pytest.fixture
+def resp_post(client, usuario):
+    return client.post(reverse('login'), {'username': usuario.email, 'password': usuario.senha_plana})
+
+
+def test_login_redirect(resp_post):
+    assert resp_post.status_code == 302
+    assert resp_post.url == reverse('modulos:indice')
+
+
+@pytest.fixture
+def resp_home(client, db):
+    return client.get(reverse('login'))
+
+
+def test_botao_entrar_disponivel(resp_home):
+    assert_contains(resp_home, 'Entrar')
+
+
+def test_link_de_login_disponivel(resp_home):
+    assert_contains(resp_home, reverse('login'))
