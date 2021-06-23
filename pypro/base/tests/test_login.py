cf26f35eba9788ebe05c0e445a8e32b0aa8b0282
Marco <miranda_garcya@hotmail.com>
2021-06-23 14:49:16 -0300

    Implememtado o Logout Close #347

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -64,5 +64,9 @@ def test_botao_sair_disponivel(resp_home_com_usuario_logado):
     assert_contains(resp_home_com_usuario_logado, 'Sair')
 
 
-#def test_nome_usuario_logado_disponivel(resp_home_com_usuario_logado, usuario_logado):
-#    assert_contains(resp_home_com_usuario_logado, usuario_logado.first_nome)
+def test_nome_usuario_logado_disponivel(resp_home_com_usuario_logado, usuario_logado):
+    assert_contains(resp_home_com_usuario_logado, usuario_logado.first_name)
+
+
+def test_link_de_logout_disponivel(resp_home_com_usuario_logado):
+    assert_contains(resp_home_com_usuario_logado, reverse('logout'))
