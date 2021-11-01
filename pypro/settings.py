5818644b86b727e26c186a032e5ad233e6ed45f3
Marco <miranda_garcya@hotmail.com>
2021-11-01 10:08:11 -0300

    Alterado os arquivos env-sample e settings para configuração de envio de email para o servidor.

--- pypro/settings.py
+++ pypro/settings.py
@@ -90,10 +90,13 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 # Configuração de envio de Emaill
 
 EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+
+DEFAUL_FROM_EMAIL = config('DEFAUL_FROM_EMAIL')
 
 # Configuração Django Debug Toolbar
 
